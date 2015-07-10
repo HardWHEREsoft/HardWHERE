348d07c4b0a260ff648b0ae65561dfb5abf193e3
Tharinda Ehelepola <tharinda221@gmail.com>
2015-07-11 02:38:43 +0530

    search item

--- HardWhere/src/main/java/com/hardwhere/controller/AddItem.java
+++
@@ -1,7 +0,0 @@
-package com.hardwhere.controller;
-
-/**
- * Created by menaka on 7/11/15.
- */
-public class AddItem {
-}
--- HardWhere/src/main/java/com/hardwhere/model/Item_POJO.java
+++
@@ -1,7 +0,0 @@
-package com.hardwhere.model;
-
-/**
- * Created by menaka on 7/11/15.
- */
-public class Item_POJO {
-}
