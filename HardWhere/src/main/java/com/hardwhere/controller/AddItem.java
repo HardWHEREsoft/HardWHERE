d1fab25e472a2b158d386f5bddb89b5910a26a84
Menaka Madushanka <menaka12350@gmail.com>
2015-07-11 00:25:10 +0530

    Item and Add Item

---
+++ HardWhere/src/main/java/com/hardwhere/controller/AddItem.java
@@ -0,0 +1,7 @@
+package com.hardwhere.controller;
+
+/**
+ * Created by menaka on 7/11/15.
+ */
+public class AddItem {
+}
---
+++ HardWhere/src/main/java/com/hardwhere/model/Item_POJO.java
@@ -0,0 +1,7 @@
+package com.hardwhere.model;
+
+/**
+ * Created by menaka on 7/11/15.
+ */
+public class Item_POJO {
+}
