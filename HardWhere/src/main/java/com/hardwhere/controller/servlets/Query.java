04270385c2b9b40484badc4bb16265fc7ca22605
Menaka Madushanka <menaka12350@gmail.com>
2015-09-19 23:25:13 +0530

    @Override

--- HardWhere/src/main/java/com/hardwhere/controller/servlets/Query.java
+++ HardWhere/src/main/java/com/hardwhere/controller/servlets/Query.java
@@ -16,11 +16,14 @@ import java.util.ArrayList;
  */
 
 public class Query  extends HttpServlet {
+
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         request.getRequestDispatcher("index.jsp").forward(request, response);
     }
 
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doPost(request, response);
     }
