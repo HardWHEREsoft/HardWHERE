c1c3e8d3d8cf4b036dbc90a04fb2fbc5596bc908
Tharinda Ehelepola <tharinda221@gmail.com>
2015-09-21 03:53:01 +0530

    Searching

--- HardWhere/web/js/additem.js
+++ HardWhere/web/js/additem.js
@@ -1,29 +1,26 @@
-/**
- * Created by menaka on 9/21/15.
- */
 var tm = 800;
 
 function selectType(){
     var t = $("#type option:selected").text();
-
+    
     if(t == "Material"){
         selectTile();
         $("#form_add").find("tr").not("#tile_brands").show(tm);
         $("#name").text("Name of Material");
     }
-
+    
     if(t == "Equipment"){
         $("#name").text("Name of Equipment");
         $("#mat_type, #tile_brands, #tile_brands").hide(tm);
         $("#form_add").find("tr").not("#mat_type, #tile_brands, #tile_brands").show(tm);
     }
-
+    
     if(t == "Service"){
-        $("#form_add").find("tr").hide(tm);
+        $("#form_add").find("tr").hide(tm);        
         $("#name").text("Name of Service");
         $("#name_tr, #desc_tr, #submit_tr, #price_tr").show(tm);
     }
-
+    
 }
 
 function selectTile(){
